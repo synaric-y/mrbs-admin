bd06b1653eee5979a500c25bce9b083e8d254b21
synaric.yong <synaric.yong@bcc.global>
2024-09-04 11:46:21 +0800

    config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
-// export const HOST = "http://localhost:81"
+export const HOST = "http://localhost:81"
 
-export const HOST = "http://172.16.89.161:82"
+// export const HOST = "http://172.16.89.161:82"
 // export const HOST = "http://127.0.0.1:81"
+
+export const STORAGE = {
+    USER_INFO: 'USER_INFO'
+}
