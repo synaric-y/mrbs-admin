affda5ce5feacb62c6e6147026a61395e062edaf
joy.cheng <joy.cheng@bcc.global>
2024-09-09 17:36:17 +0800

    会议页面去除空格

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const HOST = "http://localhost:81"
+// export const HOST = "http://localhost:81"
 
-// export const HOST = "http://172.16.89.161:82"
+export const HOST = "http://172.16.89.161:82"
 // export const HOST = "http://127.0.0.1:81"
 
 export const STORAGE = {
