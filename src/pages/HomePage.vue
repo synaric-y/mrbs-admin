6d5ddfc7846cf7e6913915ae6b5c24b298a7ebf8
joy.cheng <joy.cheng@bcc.global>
2024-09-09 18:10:43 +0800

    滚动高度

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -35,7 +35,7 @@
   </div>
 
   <div class="table-container">
-    <el-scrollbar class="scroll-table-view" always :style="{ height: (this.screenHeight - 150) + 'px' }">
+    <el-scrollbar class="scroll-table-view" always :style="{ height: (this.screenHeight - 137) + 'px' }">
       <div class="calendar-header">
         <div class="time-header">
           <div class="time-slots">
