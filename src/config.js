d10732f992e42c24c15c75f42510d73679e8e224
joy.cheng <joy.cheng@bcc.global>
2024-09-10 16:54:52 +0800

    切换为正式环境

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
+// 本地测试环境
 // export const HOST = "http://localhost:81"
-
-export const HOST = "http://172.16.89.161:82"
 // export const HOST = "http://127.0.0.1:81"
+
+// 测试环境
+// export const HOST = "http://172.16.89.161:82"
+// 正式环境
+export const HOST = "http://172.16.89.91"
+
+
 
 export const STORAGE = {
     USER_INFO: 'USER_INFO',
