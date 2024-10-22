9cd4f71b1f62704a59120cfe7a5fe908e90825ed
OcteneXin <1252239469@qq.com>
2024-10-22 16:31:20 +0800

    4个设置页面完成数据绑定、样式调整、表单校验；完成问题反馈提交页面

--- src/pages/system/SyncCalendar.vue
+++ src/pages/system/SyncCalendar.vue
@@ -4,33 +4,36 @@
       <div class="sub-page-content">
         <div class="title">日历同步</div>
 
-        <div class="form-item">
-          <span class="form-item-tip">选择服务</span>
-          <img class="form-item-img" src="../../../public/imgs/exchange.png" alt="#">
+        <div class="section">
+          <el-form :model="form" :rules="rules" ref="formRef" label-width="150px">
+
+            <el-form-item label="选择服务">
+              <img class="form-item-img" src="../../../public/imgs/exchange.png" alt="#">
+            </el-form-item>
+
+            <el-form-item prop="hosts" label="服务器地址(hosts)">
+              <el-input v-model="form.hosts" class="form-item-input" placeholder="示例:172.16.88.180" />
+              <el-button type="primary" style="margin-left: 10px;" @click="validate">测试</el-button>
+            </el-form-item>
+
+            <el-form-item prop="syncMethod" label="同步方式">
+              <el-select v-model="form.syncMethod" placeholder="Select" size="large" style="width: 340px">
+                <el-option v-for="item in groupOptions" :key="item.value" :label="item.label" :value="item.value" />
+              </el-select>
+            </el-form-item>
+
+            <el-form-item prop="syncMinute" label="同步间隔">
+              <el-select v-model="form.syncMinute" placeholder="Select" size="large" style="width: 200px;margin-right: 10px;">
+                <el-option v-for="item in everySecondOptions" :key="item.value" :label="item.label" :value="item.value" />
+              </el-select>
+            </el-form-item>
+
+          </el-form>
         </div>
 
-        <div class="form-item" style="margin-top: 46px;">
-          <span class="form-item-tip">服务器地址</span>
-          <el-input v-model="input" class="form-item-input" placeholder="示例:172.16.88.180" />
-<!--          <input class="form-item-input" placeholder="示例:172.16.88.180" />-->
-          <el-button type="primary">测试</el-button>
-        </div>
-        <div class="form-ad" style="margin-top: 46px;">
-          <span class="form-item-tip">同步方式</span>
-          <el-select v-model="groupVal" placeholder="Select" size="large" style="width: 340px">
-            <el-option v-for="item in groupOptions" :key="item.value" :label="item.label" :value="item.value" />
-          </el-select>
-        </div>
-        <div class="form-ad" style="margin-top: 46px;">
-          <span class="form-item-tip">同步间隔</span>
-          <el-select v-model="everySecondVal" placeholder="Select" size="large"
-                     style="width: 200px;margin-right: 10px;">
-            <el-option v-for="item in everySecondOptions" :key="item.value" :label="item.label" :value="item.value" />
-          </el-select>
-        </div>
         <div class="btns">
-          <el-button type="default">取消</el-button>
-          <el-button type="primary">保存</el-button>
+          <el-button >取消</el-button>
+          <el-button type="primary" @click="submit">保存</el-button>
         </div>
       </div>
     </el-main>
@@ -49,16 +52,26 @@ export default {
   mixins: [PageMixin],
   data() {
     return {
-      groupVal: '双向，Exchange与系统产生日历相互同步',
+
+      form:{
+        hosts:'',
+        syncMethod:'',
+        syncMinute:'',
+      },
+
+      rules:{
+        hosts: [{ required:true, message: '服务器地址不能为空', trigger: 'blur' }],
+        syncMethod: [{required:true, message:'请选择同步方式', trigger: 'blur'}],
+        syncMinute: [{required:true, message:'请选择同步间隔', trigger: 'blur'}]
+      },
+
       groupOptions: [
         {
           value: '双向，Exchange与系统产生日历相互同步',
           label: '双向，Exchange与系统产生日历相互同步',
         }],
-      switchVal: true,
-      radioval: 1,
-      everySecondVal: '15分钟',
-      everySecondOptions: [{
+      everySecondOptions: [
+          {
         value: '5分钟',
         label: '5分钟',
       }, {
@@ -83,7 +96,19 @@ export default {
     }
   },
   methods: {
-
+    validate(){
+      console.log(100)
+    },
+    submit(){
+      console.log(this.form)
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          console.log('submit!')
+        } else {
+          console.log('error submit!')
+        }
+      })
+    }
   },
   mounted() {
 
@@ -105,9 +130,10 @@ export default {
 }
 
 .sub-page-content{
-  width: calc(100vw - 169px);
+  width: 100%;
   height: auto;
   padding: 20px;
+  box-sizing: border-box;
   margin: 0;
   position: relative;
   font-size: 14px;
@@ -120,6 +146,11 @@ export default {
     font-family: PingFang SC;
     font-size: 20px;
     font-weight: 500;
+  }
+
+
+  .section{
+    padding-left: 50px;
   }
 
   .form-item {
@@ -153,7 +184,7 @@ export default {
   }
   .form-item-input {
     width: 400px;
-    height: 33px;
+    height: 40px;
   }
   .form-item-btn {
     border-radius: 2px;
--- src/pages/system/SyncUser.vue
+++ src/pages/system/SyncUser.vue
@@ -4,74 +4,70 @@
       <div class="sub-page-content">
         <div class="title">用户同步</div>
 
-        <div class="form-item" style="margin-top: 20px;">
-          <span class="form-item-tip">选择服务</span>
-          <img class="form-item-img" src="../../../public/imgs/ad.png" alt="#">
-        </div>
-
-        <div class="form-item">
-          <span class="form-item-tip">服务器地址(hosts)</span>
-          <el-input class="form-item-input" placeholder="示例:172.16.88.180" />
-        </div>
-
-        <div class="form-item">
-          <span class="form-item-tip">服务器端口(port)</span>
-          <el-input class="form-item-input" placeholder="示例:3136" />
-        </div>
-
-        <div class="form-item">
-          <span class="form-item-tip">基础地址(base_dn)</span>
-          <el-input class="form-item-input" placeholder="示例:0U=BCC,DC=businessconnectchina,DC=com" />
-        </div>
-
-        <div class="form-item">
-          <span class="form-item-tip">用户名(username)</span>
-          <el-input class="form-item-input" placeholder="示例:CN=meet. dap,0U=LDAP,DC=businessconnectchina,DC=com" />
-        </div>
-
-        <div class="form-item">
-          <span class="form-item-tip">密码(password)</span>
-          <el-input class="form-item-input" placeholder="请输入包含数字、字母、特殊符号最低8位密码" />
-          <el-button type="primary">测试</el-button>
-        </div>
 
 
-        <div class="form-ad">
-          <span class="form-item-tip">同步范围</span>
-          <el-tree-select v-model="adval" :data="adDatasource" multiple :render-after-expand="false" show-checkbox
-                          style="width: 240px" />
+        <div class="section">
+          <el-form :model="form" :rules="rules" ref="formRef" label-width="150px">
+
+            <el-form-item label="选择服务">
+              <img class="form-item-img" src="../../../public/imgs/ad.png" alt="#">
+            </el-form-item>
+
+            <el-form-item prop="hosts" label="服务器地址(hosts)">
+              <el-input v-model="form.hosts" class="form-item-input" placeholder="示例:172.16.88.180" />
+            </el-form-item>
+
+            <el-form-item prop="port" label="服务器端口(port)">
+              <el-input v-model="form.port" class="form-item-input" placeholder="示例:3136" />
+            </el-form-item>
+
+            <el-form-item prop="base_dn" label="基础地址(base_dn)">
+              <el-input v-model="form.base_dn" class="form-item-input" placeholder="示例:0U=BCC,DC=businessconnectchina,DC=com" />
+            </el-form-item>
+
+            <el-form-item prop="username" label="用户名(username)">
+              <el-input v-model="form.username" class="form-item-input" placeholder="示例:CN=meet. dap,0U=LDAP,DC=businessconnectchina,DC=com" />
+            </el-form-item>
+
+            <el-form-item prop="password" label="密码(password)">
+              <el-input type="password" v-model="form.password" class="form-item-input" placeholder="请输入包含数字、字母、特殊符号最低8位密码" />
+            </el-form-item>
+
+            <el-form-item prop="syncRange" label="同步范围">
+              <el-tree-select v-model="form.syncRange" :data="adDatasource" multiple :render-after-expand="false" show-checkbox style="width: 240px" />
+            </el-form-item>
+
+            <el-form-item prop="syncMethod" label="同步方式">
+              <el-select v-model="form.syncMethod" placeholder="Select" style="width: 240px;">
+                <el-option v-for="item in groupOptions" :key="item.value" :label="item.label" :value="item.value" />
+              </el-select>
+            </el-form-item>
+
+            <el-form-item prop="autoSync" label="开启定时同步">
+              <el-switch v-model="form.autoSync" />
+            </el-form-item>
+
+            <el-form-item prop="syncFrequency" label="同步间隔">
+              <el-radio-group v-model="form.syncFrequency">
+                <el-radio value="1" size="large">每隔</el-radio>
+                <el-select v-model="form.syncMinute" placeholder="Select" size="large"
+                           style="width: 100px;margin-right: 10px;">
+                  <el-option v-for="item in everySecondOptions" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+                <el-radio value="2" size="large">每天</el-radio>
+                <el-select v-model="form.syncDay" placeholder="Select" size="large" style="width: 100px;margin-right: 10px;">
+                  <el-option v-for="item in everyDayOptions" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-radio-group>
+            </el-form-item>
+
+          </el-form>
         </div>
 
-        <div class="form-ad">
-          <span class="form-item-tip">同步方式</span>
-          <el-select v-model="groupVal" placeholder="Select" size="large" style="width: 240px">
-            <el-option v-for="item in groupOptions" :key="item.value" :label="item.label" :value="item.value" />
-          </el-select>
-        </div>
-
-        <div class="form-ad">
-          <span class="form-item-tip">开启定时同步</span>
-          <el-switch v-model="switchVal" />
-        </div>
-
-        <div class="form-ad">
-          <span class="form-item-tip">同步间隔</span>
-          <el-radio-group v-model="radioval">
-            <el-radio value="1" size="large">每隔</el-radio>
-            <el-select v-model="everySecondVal" placeholder="Select" size="large"
-                       style="width: 100px;margin-right: 10px;">
-              <el-option v-for="item in everySecondOptions" :key="item.value" :label="item.label" :value="item.value" />
-            </el-select>
-            <el-radio value="2" size="large">每天</el-radio>
-            <el-select v-model="everyDayVal" placeholder="Select" size="large" style="width: 100px;margin-right: 10px;">
-              <el-option v-for="item in everyDayOptions" :key="item.value" :label="item.label" :value="item.value" />
-            </el-select>
-          </el-radio-group>
-        </div>
 
         <div class="btns">
-          <el-button type="default">取消</el-button>
-          <el-button type="primary">保存</el-button>
+          <el-button>取消</el-button>
+          <el-button type="primary" @click="submit">保存</el-button>
         </div>
       </div>
     </el-main>
@@ -90,7 +86,36 @@ export default {
   mixins: [PageMixin],
   data() {
     return {
-      adval: 'all',
+
+      form:{
+        hosts:'',
+        port:'',
+        base_dn:'',
+        username:'',
+        password:'',
+        syncRange: undefined,
+        syncMethod:'',
+        autoSync:true,
+        syncFrequency:'',
+        syncMinute:'15分钟',
+        syncDay:'06:00'
+      },
+      rules:{
+        hosts: [{ required:true, message: '服务器地址不能为空', trigger: 'blur' }],
+        port: [{ required:true, message: '端口号不能为空', trigger: 'blur' },
+                { validator: this.portValidator, message: '端口号必须是大于0且小于65536的整数', trigger: 'blur' }],
+        base_dn: [{ required:true, message: '请填写基础地址',trigger: 'blur' }],
+        username: [
+            { required:true, message: '用户名不能为空', trigger: 'blur' },
+            { min: 2, max: 30, message: '用户名的字符个数必须在2到30之间', trigger: 'blur' },
+        ],
+        password: [{ required:true, message:'请输入密码', trigger: 'blur' },
+                  {validator: this.pwdValidator, message:'密码强度过低！请输入包含数字、字母、特殊符号最低8位密码', trigger: 'blur' }
+        ],
+        syncRange: [{type:'array', required:true, message:'请选择同步范围', trigger: 'blur'}],
+        syncMethod: [{required:true, message:'请选择同步方式', trigger: 'blur'}],
+        syncFrequency: [{required:true, message:'请选择同步间隔', trigger: 'blur'}]
+      },
       adDatasource: [
         {
           value: '1',
@@ -161,66 +186,85 @@ export default {
           ],
         },
       ],
-
-      groupVal: '用户和用户组',
       groupOptions: [
         {
           value: '用户和用户组',
           label: '用户和用户组',
         }],
-      switchVal: true,
-      radioval: 1,
-      everySecondVal: '15分钟',
-      everySecondOptions: [{
-        value: '5分钟',
-        label: '5分钟',
-      }, {
-        value: '15分钟',
-        label: '15分钟',
-      }, {
-        value: '30分钟',
-        label: '30分钟',
-      }, {
-        value: '1小时',
-        label: '1小时',
-      }, {
-        value: '3小时',
-        label: '3小时',
-      }, {
-        value: '6小时',
-        label: '6小时',
-      }, {
-        value: '12小时',
-        label: '12小时',
-      }],
-
-      everyDayVal: '06:00',
-      everyDayOptions: [{
-        value: '06:00',
-        label: '06:00',
-      }, {
-        value: '08:00',
-        label: '08:00',
-      }, {
-        value: '10:00',
-        label: '10:00',
-      }, {
-        value: '12:00',
-        label: '12:00',
-      }, {
-        value: '18:00',
-        label: '18:00',
-      }, {
-        value: '20:00',
-        label: '20:00',
-      }, {
-        value: '22:00',
-        label: '22:00',
-      }],
+      everySecondOptions: [
+        {
+          value: '5分钟',
+          label: '5分钟',
+        }, {
+          value: '15分钟',
+          label: '15分钟',
+        }, {
+          value: '30分钟',
+          label: '30分钟',
+        }, {
+          value: '1小时',
+          label: '1小时',
+        }, {
+          value: '3小时',
+          label: '3小时',
+        }, {
+          value: '6小时',
+          label: '6小时',
+        }, {
+          value: '12小时',
+          label: '12小时',
+        }],
+      everyDayOptions: [
+        {
+          value: '06:00',
+          label: '06:00',
+        }, {
+          value: '08:00',
+          label: '08:00',
+        }, {
+          value: '10:00',
+          label: '10:00',
+        }, {
+          value: '12:00',
+          label: '12:00',
+        }, {
+          value: '18:00',
+          label: '18:00',
+        }, {
+          value: '20:00',
+          label: '20:00',
+        }, {
+          value: '22:00',
+          label: '22:00',
+        }],
     }
   },
   methods: {
+    pwdValidator(rule, value, callback){
+      let reg = /(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{8,20}/;
+      if(reg.test(value) === false) callback(new Error('密码强度过低！请输入包含数字、字母、特殊符号最低8位密码'))
+      else callback()
+    },
+    portValidator(rule, value, callback){
+      let reg = /^[1-9][0-9]*$/;
+      if(reg.test(value) === false) callback(new Error('端口号必须是大于0的整数'))
+      else{
+        const res = parseInt(value)
 
+        if(!res || res<=0 || res > 65535) callback(new Error('端口号必须是大于0且小于65536的整数'))
+        else callback()
+      }
+    },
+    submit(){
+      console.log(this.form)
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          console.log('submit!')
+        } else {
+          console.log('error submit!')
+        }
+      })
+    }
   },
   mounted() {
 
@@ -242,9 +286,10 @@ export default {
 }
 
 .sub-page-content{
-  width: calc(100vw - 169px);
+  width: 100%;
   height: auto;
   padding: 20px;
+  box-sizing: border-box;
   margin: 0;
   position: relative;
   font-size: 14px;
@@ -257,6 +302,10 @@ export default {
     font-family: PingFang SC;
     font-size: 20px;
     font-weight: 500;
+  }
+
+  .section{
+    padding-left: 50px;
   }
 
   .form-item {
