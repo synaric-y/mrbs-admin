0cf66f5b28b94ba099dac26f1143432b1bbe35f1
joy.cheng <joy.cheng@bcc.global>
2024-10-31 16:29:03 +0800

    二进制转化

--- src/components/CycleMeetCMP.vue
+++ src/components/CycleMeetCMP.vue
@@ -368,7 +368,7 @@ export default {
       // this.meetForm.id = this.meetForm.room_id
       this.meetForm.end_date = this.meetForm.start_date
       this.meetForm.rooms.push(this.meetForm.room_id)
-      this.meetForm.rep_opt = this.toBinary(this.meetForm.rep_day)
+      this.meetForm.rep_opt = parseInt(this.toBinary(this.meetForm.rep_day),2)
       Api.editMeet(this.meetForm).then(({ data, code, msg }) => {
         if (code == 0) {
           this.$emit('close')
@@ -386,7 +386,7 @@ export default {
     },
 
     deleteMeet() {
-      Api.deleteMeet({ entry_id: Number(this.repeat_id), entry_series: 1 }).then(({ data, code, msg }) => {
+      Api.deleteMeet({ entry_id: Number(this.entry_id), entry_series: 1 }).then(({ data, code, msg }) => {
         if (code == 0) {
           ElMessage({
             message: this.$t('base.deleteSuccess'),
